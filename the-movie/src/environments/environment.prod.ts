0f0d39589528358c748b5eb3a33c0a945066cdde
Jordi enriquez <jordi.enriquez@student.bit.es>
2020-12-28 13:41:24 +0100

    save...

--- the-movie/src/environments/environment.prod.ts
+++ the-movie/src/environments/environment.prod.ts
@@ -7,5 +7,16 @@ export const environment = {
     storageBucket: "the-movie-544f1.appspot.com",
     messagingSenderId: "562771759567",
     appId: "1:562771759567:web:1cca2787ca81ba5789c2ba"
+  },
+  contentSecurityPolicyHeader: 'Content-Security-Policy',
+  contentSecurityPolicy: {
+    'default-src': "'none'",
+    'script-src': "'self' 'unsafe-inline' http://www.google-analytics.com/analytics",
+    'font-src': "'self' https://fonts.gstatic.com",
+    'connect-src': "'self'",
+    'img-src': "'self' http://www.google-analytics.com http://trk.kissmetrics.com",
+    'style-src': "'self' https://fonts.googleapis.com",
+    'media-src': "'self'",
+    'frame-src': "'self' http://platform.twitter.com"
   }
 };
